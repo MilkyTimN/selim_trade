ef72c013fc3c2c57d8228ccd7773064fbfc3d926
Tim <nsrv.tim@gmail.com>
2023-02-15 09:02:27 +0600

    init commit

---
+++ src/main/java/kg/megalab/selim_trade/SelimTradeApplication.java
@@ -0,0 +1,13 @@
+package kg.megalab.selim_trade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelimTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelimTradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/megalab/selim_trade/SelimTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.megalab.selim_trade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelimTradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
