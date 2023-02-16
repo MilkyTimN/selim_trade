f345cfe600d6ab57e8ff7b9e98cf395d38ff4767
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-02-16 16:25:24 +0600

    Created Admin entity

---
+++ src/main/java/kg/megalab/selim_trade/entity/Admin.java
@@ -0,0 +1,31 @@
+package kg.megalab.selim_trade.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Entity
+@Table(name = "admins")
+public class Admin {
+    @Id
+    @GeneratedValue(generator = "admin_id_generator", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "admin_id_generator", sequenceName = "admin_seq", allocationSize = 1)
+    private int id;
+    private String username;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+    @CreationTimestamp
+    private LocalDate created_date;
+    @UpdateTimestamp
+    private LocalDate updated_date;
+
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "admin_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated
+    private Set<Role> roles;
+}
---
+++ src/main/java/kg/megalab/selim_trade/entity/Role.java
@@ -0,0 +1,5 @@
+package kg.megalab.selim_trade.entity;
+
+public enum Role {
+    ADMIN, SUPERADMIN
+}
