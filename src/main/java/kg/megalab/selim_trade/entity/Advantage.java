7583b467fb137cc9275313a924890657aab5a28b
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-02-16 17:08:50 +0600

    Added Advantage entity

--- src/main/java/kg/megalab/selim_trade/entity/Advantage.java
+++ src/main/java/kg/megalab/selim_trade/entity/Advantage.java
@@ -1,2 +1,29 @@
-package kg.megalab.selim_trade.entity;public class Advantage {
+package kg.megalab.selim_trade.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "advantages")
+public class Advantage {
+    @Id
+    @GeneratedValue(generator = "advantage_id_generator", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "advantage_id_generator", sequenceName = "advantage_seq", allocationSize = 1)
+    private int id;
+    private String title;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    @CreationTimestamp
+    private LocalDate created_date;
+    @UpdateTimestamp
+    private LocalDate updated_date;
+
+    @ManyToOne(fetch = FetchType.LAZY,
+    cascade = CascadeType.ALL)
+    @JoinColumn(name = "admin_id")
+    private Admin admin;
+
 }
