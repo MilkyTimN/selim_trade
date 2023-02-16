ffef3122124cc82ee798323a49ed0b57e1e449cb
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-02-16 20:42:35 +0600

    Fixed name mistake

--- src/main/java/kg/megalab/selim_trade/repository/ProductRepository.java
+++ src/main/java/kg/megalab/selim_trade/repository/ProductRepository.java
@@ -1,8 +1,9 @@
 package kg.megalab.selim_trade.repository;
 
+import kg.megalab.selim_trade.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Product extends JpaRepository<Product, Integer> {
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 }
