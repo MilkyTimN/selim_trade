9e8d734894a548058cd5dd904729da58bf39d85f
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-03-10 00:05:16 +0600

    added get all, get by id, delete by id work methods

--- src/main/java/kg/megalab/selim_trade/service/WorkService.java
+++ src/main/java/kg/megalab/selim_trade/service/WorkService.java
@@ -1,10 +1,17 @@
 package kg.megalab.selim_trade.service;
 
 import kg.megalab.selim_trade.dto.WorkResponse;
+import org.springframework.data.domain.Page;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 public interface WorkService {
     WorkResponse createWork(MultipartFile image) throws IOException;
+
+    Page<WorkResponse> getAllWorks(int pageNo, int pageSize, String sortBy);
+
+    WorkResponse getWorkById(int id);
+
+    void deleteWorkById(int id) throws IOException;
 }
