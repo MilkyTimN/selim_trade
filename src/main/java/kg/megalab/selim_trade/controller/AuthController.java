c28fdd8e9691742a48878bf71a9a2814115edf9e
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-03-21 12:23:27 +0600

    description of access and refresh tokens

--- src/main/java/kg/megalab/selim_trade/controller/AuthController.java
+++ src/main/java/kg/megalab/selim_trade/controller/AuthController.java
@@ -1,5 +1,6 @@
 package kg.megalab.selim_trade.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import jakarta.validation.Valid;
 import kg.megalab.selim_trade.dto.LoginRequest;
@@ -24,12 +25,42 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
+    @Operation(description = """
+            При логине выдается access и refresh токены.
+            С помощью access токена пользователь может авторизоваться.
+            Но он действителен только в течение 30 минут.
+            Потом чтобы не логиниться снова нужно отправить refresh токен на 
+            ендпоинт '/refresh-token'
+            все refresh токены хранятся в бд.
+            Refresh токен валиден в течение суток.
+            Если refresh токен есть в базе данных и срок его 
+            дейтсвия еще не истек, то выдается новый refresh и 
+            access токен. Старый refresh токен стирается с бд, 
+            чтобы если вдруг кто-то украдет его он не смог им воспользоваться больше 1 раза.
+            Если refresh токен тоже истек то он также стирается с бд и 
+            пользователю придется уже логиниться.
+            """)
     public LoginResponse login(@Valid @RequestBody LoginRequest loginRequest ) {
 
         return authService.login(loginRequest);
     }
 
     @PostMapping("/refresh-token")
+    @Operation(description = """
+            При логине выдается access и refresh токены.
+            С помощью access токена пользователь может авторизоваться.
+            Но он действителен только в течение 30 минут.
+            Потом чтобы не логиниться снова нужно отправить refresh токен на 
+            ендпоинт '/refresh-token'
+            все refresh токены хранятся в бд.
+            Refresh токен валиден в течение суток.
+            Если refresh токен есть в базе данных и срок его 
+            дейтсвия еще не истек, то выдается новый refresh и 
+            access токен. Старый refresh токен стирается с бд, 
+            чтобы если вдруг кто-то украдет его он не смог им воспользоваться больше 1 раза.
+            Если refresh токен тоже истек то он также стирается с бд и 
+            пользователю придется уже логиниться.
+            """)
     public LoginResponse refreshAccessToken(@Valid @RequestBody RefreshAccessTokenRequest refreshToken) {
         return authService.refreshAccessToken(refreshToken);
     }
