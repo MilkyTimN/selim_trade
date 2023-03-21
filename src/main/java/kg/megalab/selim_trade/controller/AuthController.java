37dc4cbafb92708245e71c1862225f1bd0044e50
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-03-21 12:11:35 +0600

    register request params desctiption in swagger

--- src/main/java/kg/megalab/selim_trade/controller/AuthController.java
+++ src/main/java/kg/megalab/selim_trade/controller/AuthController.java
@@ -2,10 +2,18 @@ package kg.megalab.selim_trade.controller;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import jakarta.validation.Valid;
-import kg.megalab.selim_trade.dto.*;
+import kg.megalab.selim_trade.dto.LoginRequest;
+import kg.megalab.selim_trade.dto.LoginResponse;
+import kg.megalab.selim_trade.dto.RefreshAccessTokenRequest;
+import kg.megalab.selim_trade.exceptions.BadRequestException;
 import kg.megalab.selim_trade.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,7 +24,8 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
-    public LoginResponse login(@Valid @RequestBody LoginRequest loginRequest) {
+    public LoginResponse login(@Valid @RequestBody LoginRequest loginRequest ) {
+
         return authService.login(loginRequest);
     }
 
--- src/main/java/kg/megalab/selim_trade/exceptions/RestApiExceptionHandler.java
+++ src/main/java/kg/megalab/selim_trade/exceptions/RestApiExceptionHandler.java
@@ -2,17 +2,21 @@ package kg.megalab.selim_trade.exceptions;
 
 import kg.megalab.selim_trade.dto.ErrorMessage;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
+import java.util.stream.Collectors;
 
 
 @RestControllerAdvice
-public class RestApiExceptionHandler extends ResponseEntityExceptionHandler {
+public class RestApiExceptionHandler{
 
 
     @ExceptionHandler({
@@ -45,6 +49,20 @@ public class RestApiExceptionHandler extends ResponseEntityExceptionHandler {
                 new Date(),
                 exception.getMessage(),
                 request.getDescription(false)
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorMessage badRequest(MethodArgumentNotValidException exception, WebRequest request) {
+        return new ErrorMessage(
+                HttpStatus.BAD_REQUEST.value(),
+                new Date(),
+                "Bad request",
+                exception.getBindingResult().getFieldErrors()
+                        .stream().map(FieldError::getDefaultMessage)
+                        .toList()
+                        .toString()
         );
     }
 
