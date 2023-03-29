e223bbe2c2359c2b91025aca8bdaf7953a2a8c5f
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-03-29 21:36:50 +0600

    optimized imports

--- src/main/java/kg/megalab/selim_trade/service/GateService.java
+++ src/main/java/kg/megalab/selim_trade/service/GateService.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.Optional;
 
 public interface GateService {
     GateResponse createGate(int gateTypeId, String name, MultipartFile image, UserDetails adminDetails) throws IOException;
