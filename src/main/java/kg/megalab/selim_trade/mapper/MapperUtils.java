08be3a8bd66ae84eec36b9374505035509de0960
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-04-01 22:51:16 +0600

    new class for getting usernames

--- src/main/java/kg/megalab/selim_trade/mapper/MapperUtils.java
+++ src/main/java/kg/megalab/selim_trade/mapper/MapperUtils.java
@@ -1,2 +1,17 @@
-package kg.megalab.selim_trade.mapper;public class MapperUtils {
+package kg.megalab.selim_trade.mapper;
+
+import kg.megalab.selim_trade.dto.UpdatedByResponse;
+import kg.megalab.selim_trade.entity.UpdatedBy;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MapperUtils {
+
+    public static List<UpdatedByResponse> mapUpdatedByList(List<UpdatedBy> updatedByList) {
+        return updatedByList.stream()
+                .map(updatedBy -> new UpdatedByResponse(
+                        updatedBy.getAdmin().getUsername(),
+                        updatedBy.getDate().toString())).collect(Collectors.toList());
+    }
 }
