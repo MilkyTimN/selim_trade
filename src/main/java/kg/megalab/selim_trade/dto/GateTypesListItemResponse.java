9dcd314f9f2860c0c842274dffdddb295bd33db6
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-04-11 19:41:44 +0600

    gate types list view for admins

--- src/main/java/kg/megalab/selim_trade/dto/GateTypesListItemResponse.java
+++ src/main/java/kg/megalab/selim_trade/dto/GateTypesListItemResponse.java
@@ -1,10 +1,10 @@
 package kg.megalab.selim_trade.dto;
 
-public record GateTypesListResponse(
+public record GateTypesListItemResponse(
         int id,
         String backgroundUrl,
         String name,
         String created_date,
-        AdminInfo createdBy
+        AdminInfoShort createdBy
 ) {
 }
