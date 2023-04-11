91df5863f5f06b8ae6ff4b44f1566c4c6e4ef584
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-04-11 20:43:52 +0600

    added a bunch of methods

--- src/main/java/kg/megalab/selim_trade/service/NewsService.java
+++ src/main/java/kg/megalab/selim_trade/service/NewsService.java
@@ -1,6 +1,9 @@
 package kg.megalab.selim_trade.service;
 
+import kg.megalab.selim_trade.dto.NewsListItemResponse;
 import kg.megalab.selim_trade.dto.NewsResponse;
+import kg.megalab.selim_trade.repository.projections.NewsItemView;
+import kg.megalab.selim_trade.repository.projections.NewsListView;
 import org.springframework.data.domain.Page;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,13 +14,16 @@ public interface NewsService {
 
     NewsResponse getNewsById(int id);
 
-    String getFileOriginalName(String url);
 
-    Page<NewsResponse> getAllNews(int pageNo, int pageSize, String sortBy);
+    NewsListItemResponse createNews(MultipartFile image, String title, String description, UserDetails adminDetails) throws IOException;
 
-    NewsResponse createNews(MultipartFile image, String title, String description, UserDetails adminDetails) throws IOException;
-
-    NewsResponse updateNews(int id, MultipartFile image, String title, String description, UserDetails adminDetails) throws IOException;
+    NewsListItemResponse updateNews(int id, MultipartFile image, String title, String description, UserDetails adminDetails) throws IOException;
 
     void deleteNews(int id) throws IOException;
+
+    Page<NewsListView> getAllNewsForCustomer(int pageNo, int pageSize, String sortBy);
+
+    Page<NewsListItemResponse> getAllNewsShort(int pageNo, int pageSize, String sortBy);
+
+    NewsItemView getNewsByIdForCustomer(int newsId);
 }
