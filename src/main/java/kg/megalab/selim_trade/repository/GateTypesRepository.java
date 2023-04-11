b7dc9ecad7e56d917c90f7faa00df1c120ce104e
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-04-11 20:35:05 +0600

    methods for projections

--- src/main/java/kg/megalab/selim_trade/repository/GateTypesRepository.java
+++ src/main/java/kg/megalab/selim_trade/repository/GateTypesRepository.java
@@ -1,9 +1,19 @@
 package kg.megalab.selim_trade.repository;
 
 import kg.megalab.selim_trade.entity.GateType;
+import kg.megalab.selim_trade.repository.projections.GateTypeItemView;
+import kg.megalab.selim_trade.repository.projections.GateTypesListView;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface GateTypesRepository extends JpaRepository<GateType, Integer> {
+    Page<GateTypesListView> findAllProjectedBy(Pageable pageable);
+
+    Optional<GateTypeItemView> findProjectedById(int id);
+
 }
