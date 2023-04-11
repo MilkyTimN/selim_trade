8a67da9c798f3d2e04652d92f31356126002c98b
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-04-11 20:30:11 +0600

    advantage create response was added

--- src/main/java/kg/megalab/selim_trade/mapper/AdvantageMapper.java
+++ src/main/java/kg/megalab/selim_trade/mapper/AdvantageMapper.java
@@ -1,16 +1,17 @@
 package kg.megalab.selim_trade.mapper;
 
+import kg.megalab.selim_trade.dto.AdvantageCreateResponse;
 import kg.megalab.selim_trade.dto.AdvantageResponse;
 import kg.megalab.selim_trade.dto.UpdatedByResponse;
 import kg.megalab.selim_trade.entity.Advantage;
 import kg.megalab.selim_trade.entity.UpdatedBy;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface AdvantageMapper {
+    AdvantageCreateResponse toShortDto(Advantage advantage);
 
 
     AdvantageResponse toDto(Advantage advantage);
