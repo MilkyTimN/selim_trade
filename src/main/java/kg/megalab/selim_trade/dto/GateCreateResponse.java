26e50d23984f6ab2a66a419acbc95faf2f650505
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-04-11 19:40:35 +0600

    response when creating gate

--- src/main/java/kg/megalab/selim_trade/dto/GateCreateResponse.java
+++ src/main/java/kg/megalab/selim_trade/dto/GateCreateResponse.java
@@ -1,2 +1,9 @@
-package kg.megalab.selim_trade.dto;public record GateCreateResponse() {
+package kg.megalab.selim_trade.dto;
+
+public record GateCreateResponse(
+        int id,
+        String name,
+        String photoUrl,
+        String created_date
+) {
 }
