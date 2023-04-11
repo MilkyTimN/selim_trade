871e039e618e26fb864094f8e46e3987d413994c
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-04-11 20:30:34 +0600

    toshort dto method added

--- src/main/java/kg/megalab/selim_trade/mapper/GateMapper.java
+++ src/main/java/kg/megalab/selim_trade/mapper/GateMapper.java
@@ -1,18 +1,17 @@
 package kg.megalab.selim_trade.mapper;
 
+import kg.megalab.selim_trade.dto.GateCreateResponse;
 import kg.megalab.selim_trade.dto.GateResponse;
 import kg.megalab.selim_trade.dto.UpdatedByResponse;
 import kg.megalab.selim_trade.entity.Gate;
 import kg.megalab.selim_trade.entity.UpdatedBy;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface GateMapper {
+    GateCreateResponse toShortDto(Gate gate);
 
     GateResponse toDto(Gate gate);
 
