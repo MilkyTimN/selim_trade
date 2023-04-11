8a6e4148668fbd973c158bfe8dc58eaa3f07ccc8
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-04-11 20:34:12 +0600

    projection for customer news list view

--- src/main/java/kg/megalab/selim_trade/repository/projections/NewsListView.java
+++ src/main/java/kg/megalab/selim_trade/repository/projections/NewsListView.java
@@ -1,2 +1,11 @@
-package kg.megalab.selim_trade.repository.projections;public interface NewsListView {
+package kg.megalab.selim_trade.repository.projections;
+
+public interface NewsListView {
+    int getId();
+
+    String getTitle();
+
+    String getDescription();
+
+    String getPhotoUrl();
 }
