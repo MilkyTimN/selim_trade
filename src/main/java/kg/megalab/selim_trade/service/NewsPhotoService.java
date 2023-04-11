3ef673c6f734ab417840b64789b2200778f4edc8
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-04-11 20:42:53 +0600

    news photo service

--- src/main/java/kg/megalab/selim_trade/service/NewsPhotoService.java
+++ src/main/java/kg/megalab/selim_trade/service/NewsPhotoService.java
@@ -1,2 +1,13 @@
-package kg.megalab.selim_trade.service;public interface NewsPhotoService {
+package kg.megalab.selim_trade.service;
+
+import kg.megalab.selim_trade.dto.NewsPhotoResponse;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface NewsPhotoService {
+    NewsPhotoResponse createNewsPhoto(int newsId, MultipartFile photo) throws IOException;
+
+    //TODO:SPRING banner txt
+    void deleteNewsPhoto(int newsPhotoId) throws IOException;
 }
--- src/main/java/kg/megalab/selim_trade/service/impl/NewsPhotoServiceImpl.java
+++ src/main/java/kg/megalab/selim_trade/service/impl/NewsPhotoServiceImpl.java
@@ -1,2 +1,52 @@
-package kg.megalab.selim_trade.service.impl;public class NewsPhotoServiceImpl {
+package kg.megalab.selim_trade.service.impl;
+
+import kg.megalab.selim_trade.dto.NewsPhotoResponse;
+import kg.megalab.selim_trade.entity.NewsPhoto;
+import kg.megalab.selim_trade.exceptions.ResourceNotFoundException;
+import kg.megalab.selim_trade.mapper.NewsPhotoMapper;
+import kg.megalab.selim_trade.repository.NewsPhotoRepository;
+import kg.megalab.selim_trade.repository.NewsRepository;
+import kg.megalab.selim_trade.service.ImageService;
+import kg.megalab.selim_trade.service.NewsPhotoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+//TODO:return types password
+
+@Service
+@RequiredArgsConstructor
+public class NewsPhotoServiceImpl implements NewsPhotoService {
+    private final NewsPhotoRepository newsPhotoRepository;
+    private final NewsRepository newsRepository;
+    private final ImageService imageService;
+    private final NewsPhotoMapper newsPhotoMapper;
+    @Value("${home.dir}")
+    private String home_dir;
+
+    @Override
+    public NewsPhotoResponse createNewsPhoto(int newsId, MultipartFile photo) throws IOException {
+        NewsPhoto newsPhoto = new NewsPhoto();
+        newsPhoto.setNews(
+                newsRepository.findById(newsId).orElseThrow(
+                        () -> new ResourceNotFoundException("News not found!")
+                )
+        );
+        String resultPhotoUrl = imageService.saveImageToFileSystem(photo);
+        newsPhoto.setPhotoUrl(resultPhotoUrl);
+        return newsPhotoMapper.toDto(newsPhotoRepository.save(newsPhoto));
+    }
+
+    @Override
+    public void deleteNewsPhoto(int newsPhotoId) throws IOException {
+        NewsPhoto newsPhoto = newsPhotoRepository.findById(newsPhotoId)
+                .orElseThrow(() -> new ResourceNotFoundException("No such photo!"));
+        Files.deleteIfExists(Path.of(home_dir + newsPhoto.getPhotoUrl()));
+        newsPhotoRepository.deleteById(newsPhotoId);
+    }
 }
