d2a648146263db3d4f5a6dfba92278cfe0331824
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-04-11 20:35:23 +0600

    news photo repository

--- src/main/java/kg/megalab/selim_trade/repository/NewsPhotoRepository.java
+++ src/main/java/kg/megalab/selim_trade/repository/NewsPhotoRepository.java
@@ -1,2 +1,10 @@
-package kg.megalab.selim_trade.repository;public interface NewsPhotoRepository {
+package kg.megalab.selim_trade.repository;
+
+import kg.megalab.selim_trade.entity.NewsPhoto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NewsPhotoRepository extends JpaRepository<NewsPhoto, Integer> {
+
 }
