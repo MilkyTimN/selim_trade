0fd71bc630116697ddb7872f3e491bf4d6882cad
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-04-11 20:31:25 +0600

    added methods for optimized fields

--- src/main/java/kg/megalab/selim_trade/mapper/NewsMapper.java
+++ src/main/java/kg/megalab/selim_trade/mapper/NewsMapper.java
@@ -1,17 +1,20 @@
 package kg.megalab.selim_trade.mapper;
 
+import kg.megalab.selim_trade.dto.NewsListItemResponse;
 import kg.megalab.selim_trade.dto.NewsResponse;
 import kg.megalab.selim_trade.dto.UpdatedByResponse;
 import kg.megalab.selim_trade.entity.News;
 import kg.megalab.selim_trade.entity.UpdatedBy;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface NewsMapper {
     NewsResponse toNewOrUpdatedResponse(News news);
+
+    NewsListItemResponse toShortNewsDto(News news);
+
     default List<UpdatedByResponse> mapUpdatedByList(List<UpdatedBy> updatedByList) {
         return MapperUtils.mapUpdatedByList(updatedByList);
     }
