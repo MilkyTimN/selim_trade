35b67124b7a3435b73b091df34bd5e17a11a947e
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-04-11 20:31:44 +0600

    mapper for photo of news

--- src/main/java/kg/megalab/selim_trade/mapper/NewsPhotoMapper.java
+++ src/main/java/kg/megalab/selim_trade/mapper/NewsPhotoMapper.java
@@ -1,2 +1,10 @@
-package kg.megalab.selim_trade.mapper;public interface NewsPhotoMapper {
+package kg.megalab.selim_trade.mapper;
+
+import kg.megalab.selim_trade.dto.NewsPhotoResponse;
+import kg.megalab.selim_trade.entity.NewsPhoto;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface NewsPhotoMapper {
+    NewsPhotoResponse toDto(NewsPhoto newsPhoto);
 }
